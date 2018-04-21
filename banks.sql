3639c0811c740e2f45e3ed4d8e1a84c806d3e55b
git <610039018@qq.com>
2018-04-21 11:19:59 +0800

    添加base pythone模块实现银行系统的数据库工具类封装和更新banks.sql脚本

--- banks.sql
+++ banks.sql
@@ -1,113 +1,64 @@
--- MySQL dump 10.13  Distrib 5.7.21, for Linux (x86_64)
---
--- Host: localhost    Database: banks
--- ------------------------------------------------------
--- Server version	5.7.21-1ubuntu1
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+drop database if exists banks;
+create database banks;
+use banks;
 
---
--- Table structure for table `bank`
---
+CREATE TABLE bank (
+  id int(11) primary key AUTO_INCREMENT,
+  name varchar(50) not null,
+  address varchar(50) DEFAULT NULL
+);
+alter table bank
+add constraint name_address_uniq unique(name,address);
 
-DROP TABLE IF EXISTS `bank`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `bank` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) DEFAULT NULL,
-  `address` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `name` (`name`,`address`)
-) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+INSERT INTO bank VALUES 
+(1,'中国工商银行北京分行','朝阳路11号'),
+(2,'中国工商银行南海分行','三沙路192号'),
+(3,'中国工商银行西安分行','高新四路112号'),
+(4,'中国工商银行西安分行','高新四路133'),
+(5,'中国建设银行成都分行','玉林路110号'),
+(6,'中国银行西安分行','高新六路28号');
 
---
--- Dumping data for table `bank`
---
 
-LOCK TABLES `bank` WRITE;
-/*!40000 ALTER TABLE `bank` DISABLE KEYS */;
-INSERT INTO `bank` VALUES (21,'中国工商银行北京分行','朝阳路11号'),(23,'中国工商银行南海分行','三沙路192号'),(20,'中国工商银行西安分行','高新四路112号'),(25,'中国工商银行西安分行','高新四路133'),(22,'中国建设银行成都分行','玉林路110号'),(24,'中国银行西安分行','高新六路28号');
-/*!40000 ALTER TABLE `bank` ENABLE KEYS */;
-UNLOCK TABLES;
+CREATE TABLE user (
+  id int(11) primary key AUTO_INCREMENT,
+  number varchar(18) unique,
+  name varchar(50) NOT NULL,
+  phone varchar(11)
+);
 
---
--- Table structure for table `card`
---
 
-DROP TABLE IF EXISTS `card`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `card` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `number` varchar(24) DEFAULT NULL,
-  `passwd` varchar(32) NOT NULL,
-  `money` float DEFAULT NULL,
-  `bank_id` int(11) DEFAULT NULL,
-  `user_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `number` (`number`),
-  KEY `bank_id_fk` (`bank_id`),
-  KEY `user_id_fk` (`user_id`),
-  CONSTRAINT `bank_id_fk` FOREIGN KEY (`bank_id`) REFERENCES `bank` (`id`),
-  CONSTRAINT `user_id_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+INSERT INTO user VALUES 
+(1,'101100199202048876','吴鹏','15644231987'),
+(2,'101100199303048976','吴能','17644231987'),
+(3,'101100199208248876','吴钱','18644231987'),
+(4,'101100198203048275','马云','19844231987');
 
---
--- Dumping data for table `card`
---
+CREATE TABLE card (
+  id int(11) primary key AUTO_INCREMENT,
+  number varchar(24) unique,
+  passwd varchar(32) not null,
+  money float default 10,
+  bank_id int(11) ,
+  user_id int(11),
+  CONSTRAINT bank_id_fk FOREIGN KEY (bank_id) REFERENCES bank (id),
+  CONSTRAINT user_id_fk FOREIGN KEY (user_id) REFERENCES user (id)
+);
 
-LOCK TABLES `card` WRITE;
-/*!40000 ALTER TABLE `card` DISABLE KEYS */;
-INSERT INTO `card` VALUES (9,'9199919292876521','202cb962ac59075b964b07152d234b70',10,20,1),(10,'9188819292876521','250cf8b51c773f3f8dc8b4be867a9a02',10,21,2);
-/*!40000 ALTER TABLE `card` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO card VALUES 
+(1,'9199919292876521','202cb962ac59075b964b07152d234b70',10,1,1),
+(2,'9188819292876521','250cf8b51c773f3f8dc8b4be867a9a02',10,2,2);
 
---
--- Table structure for table `user`
---
 
-DROP TABLE IF EXISTS `user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `number` varchar(18) DEFAULT NULL,
-  `name` varchar(50) NOT NULL,
-  `phone` varchar(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `number` (`number`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `user`
---
-
-LOCK TABLES `user` WRITE;
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'101100199202048876','吴鹏','15644231987'),(2,'101100199303048976','吴能','17644231987'),(3,'101100199208248876','吴钱','18644231987'),(4,'101100198203048275','马云','19844231987');
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2018-04-20 12:22:34
+create table trade
+(id int primary key auto_increment,
+ card_id int,
+ to_card_id int,
+ trade_time timestamp default current_timestamp,
+ bank_id int,
+ trade_money float,
+ trade_type int(1) comment "0 查询, 1 存款, 2 取款 , 3 转账",
+ constraint t_card_id_fk foreign key(card_id) references card(id),
+ constraint to_card_id_fk foreign key(to_card_id) references card(id),
+ constraint t_bank_id_fk foreign key(bank_id) references bank(id)
+);
